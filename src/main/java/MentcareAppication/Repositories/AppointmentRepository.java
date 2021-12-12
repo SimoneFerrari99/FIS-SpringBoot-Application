5f0b3dd0549dd1e93b5997b18087567ae2d8c1d4
Simone Ferrari <simone99ferrari99@gmail.com>
2021-12-12 21:02:08 +0100

    Implementate le rotte per Comunicazioni

--- src/main/java/MentcareAppication/Repositories/AppointmentRepository.java
+++ src/main/java/MentcareAppication/Repositories/AppointmentRepository.java
@@ -3,6 +3,10 @@ package MentcareAppication.Repositories;
 import MentcareAppication.Models.Appointment;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
     Appointment findById(long id);
+    List<Appointment> findByMedicMedicID(long id);
+    List<Appointment> findByPatientPatientID(long patientID);
 }
--- src/main/java/MentcareAppication/Repositories/CommunicationRepository.java
+++ src/main/java/MentcareAppication/Repositories/CommunicationRepository.java
@@ -3,6 +3,10 @@ package MentcareAppication.Repositories;
 import MentcareAppication.Models.Communication;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CommunicationRepository extends CrudRepository<Communication, Long> {
     Communication findById(long id);
+
+    List<Communication> findByAppointmentAppointmentID(long appointmentID);
 }
