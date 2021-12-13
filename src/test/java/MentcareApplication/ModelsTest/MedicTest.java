97d90659a88b92805f31207783003fc129b0aa82
SimoneFerrari99 <simone99ferrari99@gmail.com>
2021-12-13 18:29:31 +0100

    UnitTest per il Modello Medic

--- src/test/java/MentcareApplication/ModelsTest/MedicTest.java
+++ src/test/java/MentcareApplication/ModelsTest/MedicTest.java
@@ -1,4 +1,54 @@
 package MentcareApplication.ModelsTest;
 
+import MentcareApplication.Models.Medic;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MedicTest {
+
+    private Medic medic;
+
+    @Before
+    public void setUp(){
+        medic = new Medic("Brad", "Pitt", "Psicologo", "Svolge attività di prevenzione, diagnosi, intervento, promozione della salute, abilitazione-riabilitazione, sostegno e consulenza in ambito psicologico.");
+    }
+
+    @Test
+    public void getFirstnameTest() {
+        assertEquals("Brad", this.medic.getFirstname());
+    }
+
+    @Test
+    public void getLastnameTest() {
+        assertEquals("Pitt", this.medic.getLastname());
+    }
+
+    @Test
+    public void getSpecializationTest() {
+        assertEquals("Psicologo", this.medic.getSpecialization());
+    }
+
+    @Test
+    public void getSpecializationDescriptionTest() {
+        assertEquals("Svolge attività di prevenzione, diagnosi, intervento, promozione della salute, abilitazione-riabilitazione, sostegno e consulenza in ambito psicologico.", this.medic.getSpecializationDescription());
+    }
+
+    @Test
+    public void getFirstnameLastnameTest() {
+        assertEquals("Brad Pitt", this.medic.getFirstnameLastname());
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("Medic{" +
+                "medicID=" + medic.getMedicID() +
+                ", firstname='" + medic.getFirstname() + '\'' +
+                ", lastname='" + medic.getLastname() + '\'' +
+                ", specialization='" + medic.getSpecialization() + '\'' +
+                ", specializationDescription='" + medic.getSpecializationDescription() + '\'' +
+                '}', this.medic.toString());
+    }
+
 }
